e2604c091fee5f03c759411091c1ba809bc99ec7
Jose Masri <josemasri2@gmail.com>
2020-06-10 19:19:07 -0500

    Updates

---
+++ precache-manifest.41792ef4c35e159b47d13d9309b00032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62afad81de230988365e466c5dbb96d6",
+    "url": "/ReactMovies/index.html"
+  },
+  {
+    "revision": "8097de75234e47885b5a",
+    "url": "/ReactMovies/static/css/main.00824ff7.chunk.css"
+  },
+  {
+    "revision": "b30037066b24244b5447",
+    "url": "/ReactMovies/static/js/2.b0e64166.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ReactMovies/static/js/2.b0e64166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8097de75234e47885b5a",
+    "url": "/ReactMovies/static/js/main.db93a333.chunk.js"
+  },
+  {
+    "revision": "f825eacf2c3986d05993",
+    "url": "/ReactMovies/static/js/runtime-main.30ddca7d.js"
+  }
+]);
